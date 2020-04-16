384282a1bf652a555da0243c1d0cd126d40a50fe
German Cruz <cruzgerman216@yahoo.com>
2020-04-16 17:11:42 +0000

    Done.

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,18 @@
-# Write your code here
+require 'open-uri'
+
+class GetRequester
+  attr_accessor :url
+  def initialize(url)
+    @url = url
+    get_response_body
+  end
+
+  def get_response_body
+    uri = URI.parse(@url)
+    uri.open.string
+  end
+
+  def parse_json
+    JSON.parse(get_response_body)
+  end
+end
